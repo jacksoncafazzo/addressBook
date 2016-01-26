726724081ef0d69e6b7a39d982417449d6c4db78
Jackson Cafazzo <doctorjuno@gmail.com>
2016-01-25 23:10:33 -0800

    styles and alignments

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,7 @@ function resetFields() {
   $("input#new-first-name").val("");
   $("input#new-last-name").val("");
   $("input.new-street").val("");
-  $("input.new-cities").val("");
+  $("input.new-city").val("");
   $("input.new-state").val("");
 }
 
